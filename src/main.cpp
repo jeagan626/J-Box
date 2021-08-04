8d442da3f162c0aed118edb3d03ac0ed2425edc9
jeagan626 <71165224+jeagan626@users.noreply.github.com>
2021-08-04 02:02:41 -0700

    Added toDo

--- src/main.cpp
+++ src/main.cpp
@@ -49,7 +49,11 @@ u8g2.begin();
 void loop() {
   // put your main code here, to run repeatedly:
   // toDo have simple setup screen function to walk user through setup options
-
+  // fetch data from diffrent sources and display it to the user on screens
+  //allow user to log events or enable continous logging of all data channels
+  // have performance metric modes that test 0-60, 5-60, lateral acceleration...
+  //... acceleration by RPM (Dyno Mode)
+  // math channels that calculate MPG, Wh/mi, BSFC (MPH/S / (cc/s,G/s)) VE? (cc/s)
 
 }
 
