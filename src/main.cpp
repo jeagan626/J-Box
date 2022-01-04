fc84e6b51def22f7131fa12f884fa5ddfef831db
jeagan626 <71165224+jeagan626@users.noreply.github.com>
2022-01-04 01:12:41 -0800

    Tachometer Update
    Tachometer now reads with 1hz resolution in nearly every case. it updates quickly because it measures the time between pulses and calculates frequency based on an average of periods. May be time intensive though, but initial tests are good.
    
    RAM:   [=         ]   5.1% (used 13400 bytes from 262136 bytes)
    Flash: [===       ]  32.9% (used 172288 bytes from 524288 bytes)

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ const uint8_t numPulses = 10;
 volatile unsigned long pulseTime[numPulses] = {0}; // keep track of the last 10 pulse times
 volatile uint8_t pulseIndex = 0; // used to record to pulsetime
 unsigned int pulseDeltaTimes[numPulses]; // keep track of the last 10 pulse times
+unsigned long pulseSum = 0;
 int tachCount = 0;
 int pulseInterval = 100;
 int tachFreq = 0;
@@ -69,28 +70,44 @@ void loop() {
   //Serial.println(displayUpdateTime);
   unsigned long pulseDeltaAvg = 0;
   noInterrupts();
+  //unsigned long lastPulseDelta = 1000; // pulse delta should be about this big
+  uint8_t numPulseDeltas = 0;
   for(uint8_t i = 1; i < numPulses; i++)
   {
-    unsigned int pulseDelta = abs(pulseTime[i] - pulseTime[i-1]); // find the diffrence in time between the two pulses
-    
-    //pulseDelta = pulseDelta / 100; // devide by 100 to get int milliseconds
-    Serial.println(pulseDelta);
-    pulseDeltaAvg = pulseDelta + pulseDeltaAvg; // add the pulse delta to the running average
+    unsigned long pulseDelta = abs(pulseTime[i] - pulseTime[i-1]); // find the diffrence in time between the two pulses
+    //Serial.println(pulseDelta);
+    if(pulseDelta > 1000000 ){continue;} // go on this is garbage} //4,294,867,297 pulse delta shows up from time to time
+    //lastPulseDelta = pulseDelta;
+    pulseSum = pulseDelta + pulseSum; // add the pulse delta to the running average
+    numPulseDeltas++; // keep track of the number of deltas added to the sum so we can calculate an accurate average later
+    // Serial.print(pulseSum);
+    // Serial.print("  index:");
+    // Serial.println(i);
   }
+  Serial.println(pulseSum);
   interrupts();
+  pulseDeltaAvg = pulseSum / (numPulseDeltas); 
+  Serial.println(numPulseDeltas);
+  pulseSum = 0;
   Serial.println(pulseDeltaAvg);
-  pulseDeltaAvg = pulseDeltaAvg / (numPulses - 1); 
-  Serial.println(pulseDeltaAvg);
-
-
-  int newtachFreq = (pulseCount * 500) / 50; // measure the frequency of the tach wire (change interupt means two counts per pulse)
-   if (abs(newtachFreq - tachFreq) <= 10){ // if the diffrence between the frequencies is small
+  int newtachFreq = 1000000/pulseDeltaAvg;
+  if (abs(newtachFreq - tachFreq) <= 2){ // if the diffrence between the frequencies is small
     tachFreq = max(tachFreq,newtachFreq); // use the larger of the two calculated frequencies to prevent jitter
    }
    else
    { // use the new frequency
     tachFreq = newtachFreq;
    }
+  Serial.println(tachFreq);
+
+  // int newtachFreq = (pulseCount * 500) / 50; // measure the frequency of the tach wire (change interupt means two counts per pulse)
+  //  if (abs(newtachFreq - tachFreq) <= 10){ // if the diffrence between the frequencies is small
+  //   tachFreq = max(tachFreq,newtachFreq); // use the larger of the two calculated frequencies to prevent jitter
+  //  }
+  //  else
+  //  { // use the new frequency
+  //   tachFreq = newtachFreq;
+  //  }
   // Serial.print(pulseCount);
   // Serial.print("  ");
   // Serial.print(pulseInterval);
